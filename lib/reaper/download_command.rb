40beabee2db0ac836079ed8ef58364637b23153c
Mitchell Lee Garvin <odin@Midgard.localdomain>
2011-01-27 01:50:09 -0500

    Added anime object caching in a non DRY way, need to DRY this up later

---
+++ lib/reaper/download_command.rb
@@ -0,0 +1,49 @@
+require "clamp"
+
+class DownloadCommand < Clamp::Command
+
+  data_filename = ENV["XDG_DATA_HOME"] + "/"+ "reaper.yml"
+  has_data_file = File.exists? data_filename
+
+  parameter "[ANIME]", "Name of Anime to download" do |name|
+    if has_data_file
+      data = YAML.load_file(data_filename)
+      if data.has_key?(name)
+        data[name]
+      else
+        data[name] = Anime.new name
+        File.open(data_filename, "w") do |f|
+          YAML.dump(data,f)
+        end
+        data[name]
+      end
+    else
+      data = {}
+      data[name] = Anime.new name
+      File.open(data_filename, "w") do |f|
+        YAML.dump(data,f)
+      end
+      data[name]
+    end
+  end
+
+  parameter "[EPISODES]", "Episodes to download" do |ep|
+    if ep.class == String && ep.size > 1 &&!ep.match(/[A-z]|\./)
+      ep.split(",").map(&:to_i)
+    elsif ep.class == String && ep.size > 1 && ep.match(/\.\./)
+      ep = ep.split("..")
+      Range.new ep.first.to_i, ep.last.to_i
+    elsif ep.size == 1
+      ep.to_i
+    end
+  end
+
+  parameter "[REGEX]", "Regex to select download link" do |regex|
+    Regexp.new regex
+  end
+
+  def execute
+    download = Download.new anime, episodes, regex
+    download.reap
+  end
+end
--- lib/reaper/reaper.rb
+++ lib/reaper/reaper.rb
@@ -3,6 +3,7 @@ require "reaper/anime"
 require "reaper/download"
 require "reaper/index_command"
 require "reaper/info_command"
+require "reaper/download_command"
 
 SITE = "http://www.animecrazy.net/"
 class Reaper < Clamp::Command
@@ -11,29 +12,5 @@ class Reaper < Clamp::Command
 
   subcommand "info", "Print information about given anime", InfoCommand
 
-  subcommand "download", "Download Anime" do
-    parameter "[ANIME]", "Name of Anime to download" do |name|
-      Anime.new name if name
-    end
-
-    parameter "[EPISODES]", "Episodes to download" do |ep|
-      if ep.class == String && ep.size > 1 &&!ep.match(/[A-z]|\./)
-        ep.split(",").map(&:to_i)
-      elsif ep.class == String && ep.size > 1 && ep.match(/\.\./)
-        ep = ep.split("..")
-        Range.new ep.first.to_i, ep.last.to_i
-      elsif ep.size == 1
-        ep.to_i
-      end
-    end
-
-    parameter "[REGEX]", "Regex to select download link" do |regex|
-      Regexp.new regex
-    end
-
-    def execute
-      download = Download.new anime, episodes, regex
-      download.reap
-    end
-  end
+  subcommand "download", "Download Anime", DownloadCommand
 end
